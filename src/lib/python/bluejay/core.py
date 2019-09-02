2f1a7c3dc814130a3856f8d77818e088cd6798c2
Andrey Asadchev <asadchev@gmail.com>
2019-09-02 15:45:43 -0400

    Python Matrix

--- src/lib/python/bluejay/core.py
+++ src/lib/python/bluejay/core.py
@@ -1,1 +1,28 @@
 from bluejay_core import *
+
+import numpy
+
+class Matrix(numpy.core.numeric.ndarray):
+
+  __array_priority__ = 10.0
+
+  def __new__(subtype, *args, dtype=None, order='F'):
+    N = numpy.core.numeric
+    buffer=None
+    shape=None
+    if len(args) == 2:
+      shape = args
+    else:
+      buffer, = args
+      shape = buffer.shape
+    return N.ndarray.__new__(subtype, shape=shape, dtype=dtype, order=order, buffer=buffer)
+
+
+
+  @property
+  def rows(self):
+    return self.shape[0]
+
+  @property
+  def cols(self):
+    return self.shape[1]
